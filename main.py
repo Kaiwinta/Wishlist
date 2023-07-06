cbb6445a25a2fc2f021b1dfcf0b51f949b988183
Kaiwinta <pradouratitouan@proton.me>
2023-07-06 22:39:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,63 @@
 import tkinter as tk
 
+palette = ["#16697A","#489FB5","#82C0CC","#EDE7E3","#FFA62B"]
+
 def main():
-    root = tk.Tk()
-    root.geometry("1280x720")
+    global palette
+
+    root = tk.Tk()  
+    root.geometry("800x450+{}+{}".format(int(root.winfo_screenwidth()/2 - 400), int(root.winfo_screenheight()/2 - 225)))
 
     root.resizable(False,False)
-    root.bind('<Escape>',root.destroy)
+    root.title("Kaiwinta's Wishlist")
+    root.bind('<Escape>',lambda e: root.destroy())
+    root.configure(bg=palette[2])
 
+    def TopFrame():
+        topframe = tk.Frame(root, bg= palette[0])
+        topframe.place(relheight=0.15 , relwidth=1 , relx=0, rely=0.1)
+
+        topline  = tk.Frame(topframe, bg=palette[4])
+        topline.place(relheight=0.05 , relwidth=1 , relx=0, rely=0)
+
+        botline  = tk.Frame(topframe, bg=palette[4])
+        botline.place(relheight=0.05 , relwidth=1 , relx=0, rely=0.95)
+
+
+    def LeftFrame():
+        leftframe =tk.Frame(root, bg=palette[1])
+        leftframe.place(relheight=0.5 , relwidth=0.35 ,relx=0.1 , rely=0.4)
+
+        topline  = tk.Frame(leftframe, bg=palette[0])
+        topline.place(relheight=0.03 , relwidth=1 , relx=0, rely=0)
+
+        botline  = tk.Frame(leftframe, bg=palette[0])
+        botline.place(relheight=0.03 , relwidth=1 , relx=0, rely=0.97)
+
+
+        titleFrame =tk.Frame(leftframe, bg= palette[4])
+        titleFrame.place(relheight=0.2 , relwidth=0.9 , relx=0.05 , rely=0.09)
+
+        
+
+    def RightFrame():
+        rightframe =tk.Frame(root, bg=palette[1])
+        rightframe.place(relheight=0.5 , relwidth=0.35 ,relx=0.55, rely=0.4)
+
+        topline  = tk.Frame(rightframe, bg=palette[0])
+        topline.place(relheight=0.03 , relwidth=1 , relx=0, rely=0)
+
+        botline  = tk.Frame(rightframe, bg=palette[0])
+        botline.place(relheight=0.03 , relwidth=1 , relx=0, rely=0.97)
+
+
+        titleFrame =tk.Frame(rightframe, bg= palette[4])
+        titleFrame.place(relheight=0.2 , relwidth=0.9 , relx=0.05 , rely=0.09)
+    
+
+    TopFrame()
+    LeftFrame()
+    RightFrame()
     root.mainloop()
 
 
