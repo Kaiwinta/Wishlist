7aec06730f59ad4577f91c3e6cdb5084ae6dc7cc
Kaiwinta <pradouratitouan@proton.me>
2023-07-07 23:39:11 +0200

    Turnign txt into csv for bette compatibility

--- test.py
+++ test.py
@@ -1,8 +1,18 @@
-
+import csv
 fichier = open("testtext.txt",encoding='utf-8')
 
 liste_wish = fichier.readlines()
 
-for i in range(len(liste_wish)-1):
-    print(liste_wish[i][0])
-fichier.close()
+with open('testtext.txt') as csv_file:
+    #L'on ouvre en csv
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+
+    for row in csv_reader:
+        #L'on lis les lignes
+        if line_count == 0:
+            print(f'Column names are {", ".join(row)}')
+            line_count += 1
+        else:
+            print(f'\tThis url {row[0]} is {row[1]} and cost {row[2]}.')
+            line_count += 1
