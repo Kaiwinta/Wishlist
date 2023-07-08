8079a935b4fb835b12435c51856a8fa1e3eecce1
Kaiwinta <pradouratitouan@proton.me>
2023-07-08 14:15:35 +0200

    deleting the first test

--- test.py
+++ test.py
@@ -1,30 +1,10 @@
 import csv
 
-
-with open('test2.txt') as csv_file, open("clone.txt",'w') as clone:
-    
-    #L'on ouvre en csv
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
-
-    for row in csv_reader:
-        #L'on lis les lignes
-        clone.write(f'{row[0]},{row[1]},{row[2]}\n')
-        if line_count == 0:
-            
-            print(f'Column names are {", ".join(row)}')
-            line_count += 1
-        else:
-            print(f'{row[0]},{row[1]},{row[2]}')
-            
-            line_count += 1
-
-
-
 def creerWishList(nom,elements,titre):
     """
         Objectif:
             Créer un fichier txt contenant toutes les données 
+            Sert aussi a update les wishlist
         Variables:
             nom = nom de la wishlist
             elements = nested list des données
@@ -37,13 +17,15 @@ def creerWishList(nom,elements,titre):
         for i in range(len(elements)):
             nom.write(f'{elements[i]}\n')
 
+
 nom = 'test3'
 
 elements= [
     ["prix",'url',"nom"],
     ["p","url","nom"],
     ["p","url","nom"],
-    ["p","url","nom"]
+    ["p"]
+    
 
 ]
 titre   = ["prix obj","url","nom"]
