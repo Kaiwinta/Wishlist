384cfe39725eaec9a6afe6fcd8c8d1fb36a0a53e
Kaiwinta <pradouratitouan@proton.me>
2023-07-08 17:45:43 +0200

    Create Wishlistopener.py

---
+++ Wishlistopener.py
@@ -0,0 +1,26 @@
+import csv
+
+def Savewishlist(nom,elements):
+    """
+        Objectif:
+            Créer un fichier txt contenant toutes les données 
+            Sert aussi a update les wishlist
+        Variables:
+            nom = nom de la wishlist
+            elements = nested list des données
+    """
+
+    with open(f'{nom}.txt', 'w') as file:
+        for sublist in elements:
+            file.write(','.join(sublist) + '\n')  # Write each element of the sublist separated by commas
+
+def openWishlist(nom):
+
+    with open(f'{nom}.txt') as csvfile:
+        liste_wish = []
+        csv_reader = csv.reader(csvfile, delimiter=',')
+        for line in csv_reader:
+            liste_wish.append(line)
+
+    return liste_wish
+
