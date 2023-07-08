046a04a85e870d31b8cd707bd1972fca6175e23c
Kaiwinta <pradouratitouan@proton.me>
2023-07-08 14:11:25 +0200

    creation of wishlist

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import csv
 
 
-with open('testtext.txt') as csv_file, open("clone.txt",'w') as clone:
+with open('test2.txt') as csv_file, open("clone.txt",'w') as clone:
     
     #L'on ouvre en csv
     csv_reader = csv.reader(csv_file, delimiter=',')
@@ -9,14 +9,43 @@ with open('testtext.txt') as csv_file, open("clone.txt",'w') as clone:
 
     for row in csv_reader:
         #L'on lis les lignes
+        clone.write(f'{row[0]},{row[1]},{row[2]}\n')
         if line_count == 0:
+            
             print(f'Column names are {", ".join(row)}')
             line_count += 1
         else:
             print(f'{row[0]},{row[1]},{row[2]}')
-            clone.write(f'{row[0]},{row[1]},{row[2]}')
+            
             line_count += 1
 
 
 
-    
+def creerWishList(nom,elements,titre):
+    """
+        Objectif:
+            Créer un fichier txt contenant toutes les données 
+        Variables:
+            nom = nom de la wishlist
+            elements = nested list des données
+            titre = titre des données
+    """
+
+    with open(f'{nom}.txt','w') as nom:
+        nom.write(f'{titre[:]}\n')
+
+        for i in range(len(elements)):
+            nom.write(f'{elements[i]}\n')
+
+nom = 'test3'
+
+elements= [
+    ["prix",'url',"nom"],
+    ["p","url","nom"],
+    ["p","url","nom"],
+    ["p","url","nom"]
+
+]
+titre   = ["prix obj","url","nom"]
+
+creerWishList(nom, elements , titre)
